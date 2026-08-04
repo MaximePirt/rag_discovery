a9d9f4afcea203c7d67a150e26fa23b523798f47
Maxime <pierrot.maxime.pro@gmail.com>
2026-08-04 11:46:45 +0200

    feat(parsing): read files, create chunks
    Create interface Chunk and Filecontent
    Extract data from documents folder's files
    Separate content into chunks based on end of sentence, line or space
    tokenise data using regex which split and filter on whitespaces

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
 import * as parse_f from "./file_pars.js"
 
-
+/**
+ * @brief The main function that orchestrates files parsing, chunk creation,
+ * 		and RAG (Retrieval-Augmented Generation) processing.
+ * @returns void
+ */
 async function main() : Promise <void>
 {
 
 	try {
-		// const files =  await parse_f.listfiles("./documents");
-		// if (!files)
-		// 	throw Error
-		const files = ['sql.txt']
-		await parse_f.readfiles(files);
+		const files =  await parse_f.listfiles("./documents");
+		if (!files)
+			throw Error
+		// TODO : DEBUG variable
+		// const files = ['docker.txt']
+		const bookContent = await parse_f.readfiles(files);
+		if (!bookContent)
+			throw Error
+		const chunks: parse_f.Chunk[] = []
+		for (let i = 0; i < bookContent.length; i++)
+		{
+			const pairs = bookContent[i]
+			if (pairs == undefined)
+				continue
+			const {filename, content } = pairs
+			chunks.push(...parse_f.createChunk(content, filename))
+		}
+		console.log("Here is chunks :", chunks)
 	}
-	catch {
-		console.log("An error occured")
+	catch (error) {
+		console.log("An error occured", error)
 	}
 }
 
