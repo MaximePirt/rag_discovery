75dbbefe69bd038ddbdeb8c0fada170d41427de4
Maxime <pierrot.maxime.pro@gmail.com>
2026-08-06 20:48:54 +0200

    feat(tf-idf): create frequency functions
    Create tf-idf.ts
    add DataChunk interface : chunk extend adding our tf result
    add tokenCount and tokenFrequency
    
    index.ts:
    separate main in two blocks : first one to handle documents
    second one which handle tf-idf

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as parse_f from "./file_pars.js"
+import * as rag_f from "./tf-idf.js"
 
 /**
  * @brief The main function that orchestrates files parsing, chunk creation,
@@ -9,11 +10,13 @@ async function main() : Promise <void>
 {
 
 	try {
-		const files =  await parse_f.listfiles("./documents");
-		if (!files)
-			throw Error
+//------------------------- Parse documents and create chunks
+		// const files =  await parse_f.listfiles("./documents");
+		// if (!files)
+		// 	throw Error
 		// TODO : DEBUG variable
-		// const files = ['docker.txt']
+
+		const files = ['test.txt']
 		const bookContent = await parse_f.readfiles(files);
 		if (!bookContent)
 			throw Error
@@ -27,6 +30,20 @@ async function main() : Promise <void>
 			chunks.push(...parse_f.createChunk(content, filename))
 		}
 		console.log("Here is chunks :", chunks)
+
+//------------------------- Calculate TF-IDF
+
+		const calcChunk: rag_f.DataChunk[] = []
+		for (const i of chunks)
+		{
+			const frequency = rag_f.tokenFrequency(i.tokens)
+			console.log("Here is frequency for chunk", i.id, ":", frequency)
+			calcChunk.push({ ...i, tf: frequency })
+		}
+		console.log("Here is calcChunk :", calcChunk)
+
+
+
 	}
 	catch (error) {
 		console.log("An error occured", error)
---
+++ src/tf-idf.ts
@@ -0,0 +1,52 @@
+import * as parse_f from "./file_pars.js"
+
+/**
+ * @brief Extends the Chunk interface from file_pars.ts to include term frequency (tf) information.
+ * @interface DataChunk
+ * @extends {parse_f.Chunk}
+ * @property {Map<string, number>} tf - A map where keys are tokens and values are their respective term frequencies.
+ */
+export interface DataChunk extends parse_f.Chunk {
+	tf: Map<string, number>;
+}
+
+/**
+ * @brief Counts the occurrences of each token in the given array of tokens.
+ * @param tokens 
+ * @returns Map<string, number> A map where keys are tokens and values are their respective counts.
+ */
+function tokenCount(tokens: string[]): Map<string, number> {
+	let frequency = new Map<string, number>();
+
+	for (const term of tokens)
+	{
+		const currentCount = frequency.get(term) ?? 0;
+		frequency.set(term, currentCount + 1);
+	}
+	
+	return frequency;
+}
+
+/**
+ * @brief Calculates the term frequency (TF) for each token in the given array of tokens.
+ * @param tokens 
+ * @returns Map<string, number> A map where keys are tokens and values are their respective term frequencies.
+ */
+function tokenFrequency(tokens: string[]): Map<string, number> {
+	const frequency = tokenCount(tokens);
+	const length = tokens.length;
+	const res = new Map<string, number>();
+	if (length === 0) {
+		return res;
+	}
+	for (const [term, count] of frequency) {
+		res.set(term, count / length);
+	}
+
+	return res;
+}
+
+export {
+		tokenCount,
+		tokenFrequency,
+	}
